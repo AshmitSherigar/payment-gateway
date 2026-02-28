d9e80f39b3686f35a10cabfdacbb16ea1bb69dde
AshmitSherigar <sherigarashmit@gmail.com>
2026-02-28 15:09:44 +0530

    intial commit for backend and pool connection

---
+++ backend/src/config/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: 'payment-gateway',
+  waitForConnections: true, // wait for connections if there is a limit
+  connectionLimit: 10, // the limit for how many connections that can exist
+  queueLimit: 0, // how many can wait in queue
+});
+
+module.exports = pool;
